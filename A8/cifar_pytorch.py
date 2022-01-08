1575a2e8a08a983336e60bb4249856dd09929176
Rahul Jain <rahul.jain@mobiledgex.com>
2022-01-08 20:45:49 +0530

    A8

--- A8/cifar_pytorch.py
+++ A8/cifar_pytorch.py
@@ -169,11 +169,9 @@ trainer = Trainer(
     module_file_args=args,
     data_module_args=data_module_args,
     trainer_args=trainer_args
-    #unit test if True runs once or specifify int, use debug training/validation loop, use test if everything is working fine
     fast_dev_run=True
     precision=16,
     accelerator="dpp",
-    #overfit on small portion of data, if doesn't overfit then it won't work with large datasets
     overfit_batches=0.01
 )
 
